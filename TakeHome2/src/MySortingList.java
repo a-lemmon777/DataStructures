87006c2e9f5f1dbed5ff739fd6583f38901649fd
Aaron Lemmon <lemmo031@morris.umn.edu>
2014-10-29 21:05:08 -0500

    For TakeHome2: add, iterator, and remove all work

--- TakeHome2/src/MySortingList.java
+++ TakeHome2/src/MySortingList.java
@@ -4,7 +4,7 @@ import java.util.NoSuchElementException;
 
 public class MySortingList<E extends Comparable<E>> implements SortingList<E> {
 
-	private Node head = null;
+	private Node head = new Node(null, null, null); // Dummy starting node (helps eliminate treating start of list as a special case)
 	private int count = 0;
 	private int uniqueValueCount = 0;
 
@@ -28,35 +28,16 @@ public class MySortingList<E extends Comparable<E>> implements SortingList<E> {
 
 	@Override
 	public void add(E item) {
-//		Node mainBranchNode = head;
-		Node previous = null;
-		Node current = head;
-		while ((current != null) && (current.value.compareTo(item) < 0)) {
-			previous = current;
-			current = current.nextGreater;
-		}
-		if (previous == null) { // This means we insert at the beginning of the list
-			if (current == null) { // Insert into empty list
-				head = new Node(null, null, item);
-			} else if (current.value.compareTo(item) == 0) {
-				while(current.nextEqual != null) {
-					current = current.nextEqual;
-				}
-				current.nextEqual = new Node(null, null, item);
-			} else {
-				head = new Node(current, null, item);
-			}
-		} else if (current == null) { // Insert after end of list
-			previous.nextGreater = new Node(null, null, item);
-		} else { 
-			if (current.value.compareTo(item) == 0) { // Add an equal element
-				while(current.nextEqual != null) {
-					current = current.nextEqual;
-				}
-				current.nextEqual = new Node(null, null, item);
-			} else { // Insert node into main branch
-				previous.nextGreater = new Node(current, null, item);
-			}
+		Node before = findLastNodeBefore(item);
+		if (before.value != null && before.value.compareTo(item) == 0)  {
+			// Add the new node onto the offshoot branch of equal items
+			before.nextEqual = new Node(null, null, item);
+			count++;
+		} else {
+			// Insert the new node into the correct place in the main branch.
+			before.nextGreater = new Node(before.nextGreater, null, item);
+			count++;
+			uniqueValueCount++;
 		}
 	}
 
@@ -74,8 +55,38 @@ public class MySortingList<E extends Comparable<E>> implements SortingList<E> {
 
 	@Override
 	public void remove(int index) throws ListIndexOutOfBoundsException {
-		// TODO Auto-generated method stub
-		
+		if (index < 0) {
+			throw new ListIndexOutOfBoundsException("In the function remove, the index " + index + " was too low.");
+		} else if (count <= index) {
+			throw new ListIndexOutOfBoundsException("In the function remove, the index " + index + " was too high.");
+		}
+		// If we get this far, we know the index is within bounds
+		MySortingListIterator iterator = new MySortingListIterator();
+		// Find the previous and previousMainBranch nodes right before the node to be removed
+		for (int i = 0; i < index; i++) {
+			iterator.next();
+		}
+		Node previousMainBranch = iterator.mainBranchNode;
+		Node previous = iterator.current;
+		iterator.next();
+		Node targetForRemoval = iterator.current;
+		if (previous.value != null && previous.value.compareTo(targetForRemoval.value) == 0) {
+			// Remove from an offshoot branch (but not the root of it)
+			previous.nextEqual = targetForRemoval.nextEqual;
+			count--;
+		} else { // If we get here we know that previous.value == null(it's the dummy node) or previous doesn't "equal" target (not in the same offshoot branch)
+			if (targetForRemoval.nextEqual != null) {
+				// Remove from the root of the offshoot branch
+				targetForRemoval.nextEqual.nextGreater = targetForRemoval.nextGreater;
+				previousMainBranch.nextGreater = targetForRemoval.nextEqual;
+				count--;
+			} else {
+				// Remove a unique element
+				previous.nextGreater = targetForRemoval.nextGreater;
+				count--;
+				uniqueValueCount--;
+			}
+		}
 	}
 
 	@Override
@@ -89,19 +100,37 @@ public class MySortingList<E extends Comparable<E>> implements SortingList<E> {
 		return new MySortingListIterator();
 	}
 
+	/*
+	 * Gets the node before where item's node would go when inserted. If values equal to item already exist, this will return
+	 * last of those values. If no values equal to item are in the list, this will return the node that should precede it in
+	 * the main branch.
+	 */
+	private Node findLastNodeBefore(E item) {
+		Node toReturn = head; // the dummy node
+		// Travels along the main branch and finds the last node with a value <= item.
+		while (toReturn.nextGreater != null && toReturn.nextGreater.value.compareTo(item) <= 0) {
+			toReturn = toReturn.nextGreater;
+		}
+		if (toReturn.value != null && toReturn.value.compareTo(item) == 0) {
+			while (toReturn.nextEqual != null) {
+				toReturn = toReturn.nextEqual;
+			}
+		}
+		return toReturn;
+	}
+
 	private class MySortingListIterator implements Iterator<E> {
-		private Node mainBranchNode = head;
-		private Node current = head;
+		private Node mainBranchNode = head; // Gets us past the dummy starting node
+		private Node current = head; // Gets us past the dummy starting node
 	
 		@Override
 		public boolean hasNext() {
-			return (current != null);
+			return (current.nextEqual != null || mainBranchNode.nextGreater != null);
 		}
 	
 		@Override
 		public E next() {
 			if (hasNext()) {
-				E toReturn = current.value;
 				// Changes current to the next "equal" node if there is one, otherwise changes it to the next greater node.
 				if (current.nextEqual != null) {
 					current = current.nextEqual;
@@ -109,7 +138,7 @@ public class MySortingList<E extends Comparable<E>> implements SortingList<E> {
 					mainBranchNode = mainBranchNode.nextGreater;
 					current = mainBranchNode;
 				}
-				return toReturn;
+				return current.value;
 			} else {
 				throw new NoSuchElementException();
 			}
--- TakeHome2/src/TestMySortingList.java
+++ TakeHome2/src/TestMySortingList.java
@@ -3,17 +3,29 @@ import java.util.Iterator;
 
 public class TestMySortingList {
 
-	public static void main(String[] args) {
-		addAndIteratorWorkWithThreeUniqueValuesInOrder();
-		addAndIteratorWorkWithThreeUniqueValuesOutOfOrder();
-		addAndIteratorWorkWithSomeSimilarValuesAccordingToCompareTo();
+	public static void main(String[] args) throws ListIndexOutOfBoundsException {
+		addAndIteratorWorkWithThreeUniqueElementsInOrder();
+		addAndIteratorWorkWithThreeUniqueElementsOutOfOrder();
+		addAndIteratorWorkWithSomeSimilarElementsAccordingToCompareTo();
 		IteratorWorksOnEmptyList();
-		addAndIteratorWorkWithOneValue();
-		addAndIteratorWorkWithAllSimilarValuesAccordingToCompareTo();
+		addAndIteratorWorkWithOneElement();
+		addAndIteratorWorkWithAllSimilarElementsAccordingToCompareTo();
+		canRemoveFromOneElementList();
+		canRemoveFromBeginningOfTwoElementListWithEqualElements();
+		canRemoveFromBeginningOfTwoElementListWithUnequalElements();
+		canRemoveFromEndOfList();
+		canRemoveFromEndOfListWithTwoEqualElementsAtEnd();
+		canRemoveFirstInstanceOfMultipleEqualElementsFromMiddleOfList();
+		canRemoveMiddleInstanceOfMultipleEqualElementsFromMiddleOfList();
+		canRemoveLastInstanceOfMultipleEqualElementsFromMiddleOfList();
+		canRemoveUniqueElementFromMiddleOfList();
+		removeThrowsExceptionWhenIndexIsTooLow();
+		removeThrowsExceptionWhenIndexIsTooHigh();
+		
 	}
 
-	private static void addAndIteratorWorkWithThreeUniqueValuesInOrder() {
-		System.out.print("Both add and the iterator work with three unique values in order: ");
+	private static void addAndIteratorWorkWithThreeUniqueElementsInOrder() {
+		System.out.print("Both add and the iterator work with three unique elements in order: ");
 		SortingList<BingoPair> myList = new MySortingList<BingoPair>();
 		myList.add(new BingoPair('I', 5));
 		myList.add(new BingoPair('B', 10));
@@ -22,8 +34,8 @@ public class TestMySortingList {
 		System.out.println(arrayAndListAreEqual(expectedContents, myList));
 	}
 
-	private static void addAndIteratorWorkWithThreeUniqueValuesOutOfOrder() {
-		System.out.print("Both add and the iterator work with three unique values out of order: ");
+	private static void addAndIteratorWorkWithThreeUniqueElementsOutOfOrder() {
+		System.out.print("Both add and the iterator work with three unique elements out of order: ");
 		SortingList<BingoPair> myList = new MySortingList<BingoPair>();
 		myList.add(new BingoPair('O', 25));
 		myList.add(new BingoPair('B', 10));
@@ -32,8 +44,8 @@ public class TestMySortingList {
 		System.out.println(arrayAndListAreEqual(expectedContents, myList));
 	}
 
-	private static void addAndIteratorWorkWithSomeSimilarValuesAccordingToCompareTo() {
-		System.out.print("Both add and the iterator work with some similar values (according to compareTo): ");
+	private static void addAndIteratorWorkWithSomeSimilarElementsAccordingToCompareTo() {
+		System.out.print("Both add and the iterator work with some similar elements (according to compareTo): ");
 		SortingList<BingoPair> myList = new MySortingList<BingoPair>();
 		myList.add(new BingoPair('O', 25));
 		myList.add(new BingoPair('B', 10));
@@ -52,16 +64,16 @@ public class TestMySortingList {
 		System.out.println(arrayAndListAreEqual(expectedContents, myList));
 	}
 
-	private static void addAndIteratorWorkWithOneValue() {
-		System.out.print("Both add and the iterator work with one value: ");
+	private static void addAndIteratorWorkWithOneElement() {
+		System.out.print("Both add and the iterator work with one element: ");
 		SortingList<BingoPair> myList = new MySortingList<BingoPair>();
 		myList.add(new BingoPair('I', 5));
 		BingoPair[] expectedContents = {new BingoPair('I', 5)};
 		System.out.println(arrayAndListAreEqual(expectedContents, myList));
 	}
 
-	private static void addAndIteratorWorkWithAllSimilarValuesAccordingToCompareTo() {
-		System.out.print("Both add and the iterator work with all similar values (according to compareTo): ");
+	private static void addAndIteratorWorkWithAllSimilarElementsAccordingToCompareTo() {
+		System.out.print("Both add and the iterator work with all similar elements (according to compareTo): ");
 		SortingList<BingoPair> myList = new MySortingList<BingoPair>();
 		myList.add(new BingoPair('B', 7));
 		myList.add(new BingoPair('I', 7));
@@ -71,6 +83,143 @@ public class TestMySortingList {
 		BingoPair[] expectedContents = {new BingoPair('B', 7), new BingoPair('I', 7), new BingoPair('N', 7), new BingoPair('G', 7), new BingoPair('O', 7)};
 		System.out.println(arrayAndListAreEqual(expectedContents, myList));
 	}
+	
+	private static void canRemoveFromOneElementList() throws ListIndexOutOfBoundsException {
+		System.out.print("Can remove from one element list: ");
+		SortingList<BingoPair> myList = new MySortingList<BingoPair>();
+		myList.add(new BingoPair('I', 5));
+		myList.remove(0);
+		BingoPair[] expectedContents = {};
+		System.out.println(arrayAndListAreEqual(expectedContents, myList));
+	}
+
+	private static void canRemoveFromBeginningOfTwoElementListWithEqualElements() throws ListIndexOutOfBoundsException {
+		System.out.print("Can remove from beginning of two element list with equal elements: ");
+		SortingList<BingoPair> myList = new MySortingList<BingoPair>();
+		myList.add(new BingoPair('I', 5));
+		myList.add(new BingoPair('G', 5));
+		myList.remove(0);
+		BingoPair[] expectedContents = {new BingoPair('G', 5)};
+		System.out.println(arrayAndListAreEqual(expectedContents, myList));
+	}
+
+	private static void canRemoveFromBeginningOfTwoElementListWithUnequalElements() throws ListIndexOutOfBoundsException {
+		System.out.print("Can remove from beginning of two element list with unequal elements: ");
+		SortingList<BingoPair> myList = new MySortingList<BingoPair>();
+		myList.add(new BingoPair('I', 12));
+		myList.add(new BingoPair('G', 5));
+		myList.remove(0);
+		BingoPair[] expectedContents = {new BingoPair('I', 12)};
+		System.out.println(arrayAndListAreEqual(expectedContents, myList));
+	}
+
+	private static void canRemoveFromEndOfList() throws ListIndexOutOfBoundsException {
+		System.out.print("Can remove from end of list: ");
+		SortingList<BingoPair> myList = new MySortingList<BingoPair>();
+		myList.add(new BingoPair('O', 25));
+		myList.add(new BingoPair('B', 10));
+		myList.add(new BingoPair('I', 5));
+		myList.remove(2);
+		BingoPair[] expectedContents = {new BingoPair('I', 5), new BingoPair('B', 10)};
+		System.out.println(arrayAndListAreEqual(expectedContents, myList));
+	}
+
+	private static void canRemoveFromEndOfListWithTwoEqualElementsAtEnd() throws ListIndexOutOfBoundsException {
+		System.out.print("Can remove from end of list: ");
+		SortingList<BingoPair> myList = new MySortingList<BingoPair>();
+		myList.add(new BingoPair('O', 25));
+		myList.add(new BingoPair('B', 10));
+		myList.add(new BingoPair('N', 25));
+		myList.add(new BingoPair('I', 5));
+		myList.remove(3);
+		BingoPair[] expectedContents = {new BingoPair('I', 5), new BingoPair('B', 10), new BingoPair('O', 25)};
+		System.out.println(arrayAndListAreEqual(expectedContents, myList));
+	}
+
+	private static void canRemoveFirstInstanceOfMultipleEqualElementsFromMiddleOfList() throws ListIndexOutOfBoundsException {
+		System.out.print("Can remove first instance of multiple equal elements from middle of list: ");
+		SortingList<BingoPair> myList = new MySortingList<BingoPair>();
+		myList.add(new BingoPair('G', 10)); // First middle element
+		myList.add(new BingoPair('O', 25));
+		myList.add(new BingoPair('B', 10)); // Second middle element
+		myList.add(new BingoPair('I', 5));
+		myList.add(new BingoPair('N', 10)); // Third middle element
+		myList.add(new BingoPair('G', 25));
+		myList.add(new BingoPair('O', 5));
+		myList.remove(2);
+		BingoPair[] expectedContents = {new BingoPair('I', 5), new BingoPair('O', 5), new BingoPair('B', 10), new BingoPair('N', 10), new BingoPair('O', 25), new BingoPair('G', 25)};
+		System.out.println(arrayAndListAreEqual(expectedContents, myList));
+	}
+
+	private static void canRemoveMiddleInstanceOfMultipleEqualElementsFromMiddleOfList() throws ListIndexOutOfBoundsException {
+		System.out.print("Can remove middle instance of multiple equal elements from middle of list: ");
+		SortingList<BingoPair> myList = new MySortingList<BingoPair>();
+		myList.add(new BingoPair('G', 10)); // First middle element
+		myList.add(new BingoPair('O', 25));
+		myList.add(new BingoPair('B', 10)); // Second middle element
+		myList.add(new BingoPair('I', 5));
+		myList.add(new BingoPair('N', 10)); // Third middle element
+		myList.add(new BingoPair('G', 25));
+		myList.add(new BingoPair('O', 5));
+		myList.remove(3);
+		BingoPair[] expectedContents = {new BingoPair('I', 5), new BingoPair('O', 5), new BingoPair('G', 10), new BingoPair('N', 10), new BingoPair('O', 25), new BingoPair('G', 25)};
+		System.out.println(arrayAndListAreEqual(expectedContents, myList));
+	}
+
+	private static void canRemoveLastInstanceOfMultipleEqualElementsFromMiddleOfList() throws ListIndexOutOfBoundsException {
+		System.out.print("Can remove last instance of multiple equal elements from middle of list: ");
+		SortingList<BingoPair> myList = new MySortingList<BingoPair>();
+		myList.add(new BingoPair('G', 10)); // First middle element
+		myList.add(new BingoPair('O', 25));
+		myList.add(new BingoPair('B', 10)); // Second middle element
+		myList.add(new BingoPair('I', 5));
+		myList.add(new BingoPair('N', 10)); // Third middle element
+		myList.add(new BingoPair('G', 25));
+		myList.add(new BingoPair('O', 5));
+		myList.remove(4);
+		BingoPair[] expectedContents = {new BingoPair('I', 5), new BingoPair('O', 5), new BingoPair('G', 10), new BingoPair('B', 10), new BingoPair('O', 25), new BingoPair('G', 25)};
+		System.out.println(arrayAndListAreEqual(expectedContents, myList));
+	}
+
+	private static void canRemoveUniqueElementFromMiddleOfList() throws ListIndexOutOfBoundsException {
+		System.out.print("Can remove unique element from middle of list: ");
+		SortingList<BingoPair> myList = new MySortingList<BingoPair>();
+		myList.add(new BingoPair('O', 25));
+		myList.add(new BingoPair('B', 10));
+		myList.add(new BingoPair('I', 5));
+		myList.remove(1);
+		BingoPair[] expectedContents = {new BingoPair('I', 5), new BingoPair('O', 25)};
+		System.out.println(arrayAndListAreEqual(expectedContents, myList));
+	}
+
+	private static void removeThrowsExceptionWhenIndexIsTooLow() {
+		System.out.print("Remove throws exception when index is too low: ");
+		SortingList<BingoPair> myList = new MySortingList<BingoPair>();
+		myList.add(new BingoPair('O', 25));
+		myList.add(new BingoPair('B', 10));
+		myList.add(new BingoPair('I', 5));
+		try {
+			myList.remove(-1);
+			System.out.println(false);
+		} catch (ListIndexOutOfBoundsException e) {
+			System.out.println(true);
+		}
+	}
+
+	private static void removeThrowsExceptionWhenIndexIsTooHigh() {
+		System.out.print("Remove throws exception when index is too high: ");
+		SortingList<BingoPair> myList = new MySortingList<BingoPair>();
+		myList.add(new BingoPair('O', 25));
+		myList.add(new BingoPair('B', 10));
+		myList.add(new BingoPair('I', 5));
+		try {
+			myList.remove(3);
+			System.out.println(false);
+		} catch (ListIndexOutOfBoundsException e) {
+			System.out.println(true);
+		}
+	}
+
 	/**
 	 * Used to make sure the list is constructed correctly
 	 */
